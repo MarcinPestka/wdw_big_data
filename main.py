0dd5cdb9fe5c42d92b89420868652d204eeb0c99
Jakub Prach <jakub.prach76@gmail.com>
2023-01-21 15:17:47 +0100

    fix for salary

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ job_offers = []
 offers = soup.find_all(class_='MuiBox-root css-6vg4fr')
 for offer in offers:
     job_name = offer.find('h2', class_='css-g9dzcj').get_text()
-    salary = re.search(r'\d+\s\d+', offer.find(class_='css-lz8wxo').get_text()).group()
+    salary = re.findall(r'\d+\s\d+', offer.find(class_='css-lz8wxo').get_text())
     job_offers.append({'jobName': job_name, 'salary': salary})
 
 
